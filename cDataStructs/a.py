950bd7fc1c44c10e517a80e893b516d9c01fd031
vitrobiani <vitrobiani@gmail.com>
2024-03-01 00:54:36 +0200

    jtp

---
+++ cDataStructs/a.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import pygame
+#from PyOpenGL.gl import *
+#from PyOpenGL.glu import *
+
+
+pygame.init()
+
+SCREEN_SIZE = (640, 480)
+
+screen = pygame.display.set_mode(SCREEN_SIZE)
+
+
